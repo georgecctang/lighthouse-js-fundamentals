f0fb66428930292b26ebcdda07a9b317b307eac3
George Tang <georgecctang@gmail.com>
2020-08-30 20:32:07 +0000

    use while loop to  print packging array

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,18 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//use for loop
 console.log("Kitchen stuff to pack:");
 
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// use while loop
+console.log("Kitchen stuff to pack:");
+
+let i = 0;
+
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
